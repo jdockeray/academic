dc1f1fe0a8f2fd9a5c9ee4f6296ad0eefa86093e
James Dockeray <james.dockeray@red-badger.com>
2023-04-11 09:24:00 +0100

    tidied up and improved runtime

--- src/dev/dockeray/leetcode/trees/CompleteTree.java
+++ src/dev/dockeray/leetcode/trees/CompleteTree.java
@@ -9,48 +9,33 @@ public class CompleteTree {
     public boolean isCompleteTree(TreeNode root) {
         List<TreeNode> level = new ArrayList<>();
         boolean valid = true;
+        var nullPresent = false;
 
         level.add(root);
-        var nullPresent = false;
         while (!level.isEmpty() && valid) {
+            var nextLevel = new ArrayList<TreeNode>();
 
-            var vals = level.stream().flatMap(l -> {
-                List<Boolean> next = new ArrayList<>();
-                next.add(l.left != null);
-                next.add(l.right != null);
-                return next.stream();
-            }).toList();
-
-            var isLastLevel = level.stream().filter(t -> t.right != null || t.left != null).toList().size() == 0;
-            for (Boolean current : vals) {
-                if (nullPresent && current) {
-
-                    valid = false;
-
+            for(TreeNode t : level) {
+                boolean isLeftNull = t.left == null;
+                boolean isRightNull = t.right == null;
+                boolean[] values = {!isLeftNull, !isRightNull};
+                if(!isLeftNull){
+                    nextLevel.add(t.left);
                 }
-//                if(!current && !isLastLevel){
-//                    valid = false;
-//                }
-
-                if (!current) {
-                    nullPresent = true;
+                if(!isRightNull){
+                    nextLevel.add(t.right);
+                }
+                for (Boolean current : values) {
+                    if (nullPresent && current) {
+                        valid = false;
+                    }
+                    if (!current) {
+                        nullPresent = true;
+                    }
                 }
             }
 
-            if (!isLastLevel) {
-                level = level.stream().flatMap(l -> {
-                    List<TreeNode> next = new ArrayList<>();
-                    if (l.left != null) {
-                        next.add(l.left);
-                    }
-                    if (l.right != null) {
-                        next.add(l.right);
-                    }
-                    return next.stream();
-                }).toList();
-            } else {
-                level = new ArrayList<>();
-            }
+            level = nextLevel;
         }
         return valid;
     }
